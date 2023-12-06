e8d4ee5135b0b72b5301827b8874144ea463f68f
Lewis Myllylä <yllillilly9@gmail.com>
2023-12-06 10:50:18 +0100

    a

--- Assets/Scripts/Threatkill.cs
+++ Assets/Scripts/Threatkill.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
+
 
 public class Threatkill : MonoBehaviour
 
 {
-
+    private Rigidbody2D _rb2D;
     private Animator _ani;
+    private float _mass = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Interacts with Rigidbody.
+        _rb2D = GetComponent<Rigidbody2D>();
+        _ani = GetComponent<Animator>();
+
+        _mass = _rb2D.mass;
 
     }
 
@@ -28,8 +36,26 @@ public class Threatkill : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(other.gameObject);
+
         }
 
+    }  
+    
+        private bool _once = false;
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !_once)
+        {
+            _once = true;
+            GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreHandler>().AddScore();
+        }
     }
 }
+
+
+
+
+
+
+
